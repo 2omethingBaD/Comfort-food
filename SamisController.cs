965a7033a50d2031ec7e1e0fec15e10211a4918a
2omethingBaD <113650499+2omethingBaD@users.noreply.github.com>
2023-03-11 23:43:19 -0500

    added interaction indicator

--- SamisController.cs
+++ SamisController.cs
@@ -6,8 +6,10 @@ public class SamisController : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 5f;  //sets player speed
     public Rigidbody2D _rb; //sets up variable for rigid body
-    public Vector2 _move;
- 
+    public Vector2 _move;//sets up the player movement
+    //to set the specific game object to manipulate
+    public GameObject _saveInteract;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,17 +19,40 @@ public class SamisController : MonoBehaviour
     }
 
 
+    //executes when player leaves a trigger area 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //removes talk indicator(specifically for the save trigger)
+        if (collision.gameObject.name == "saveTrig")
+        {
+            _saveInteract.SetActive(false);
+        }
+    }
+
+
+    //executes when player enters a trigger area 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //adds talk indicator(specifically for the save trigger)
+        if (collision.gameObject.name == "saveTrig")
+        {
+            _saveInteract.SetActive(true);
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        //player movement
+        //checks for player movement
         _move.x = Input.GetAxisRaw("Horizontal");
         _move.y = Input.GetAxisRaw("Vertical");
     }
 
 
+    //is called every 50 frames(better for physics stuffs)
     void FixedUpdate()
     {
-        _rb.MovePosition(_rb.position + _move * playerSpeed * Time.fixedDeltaTime);
+        _rb.MovePosition(_rb.position + _move * playerSpeed * Time.fixedDeltaTime);//actually changes your position
     }
 }
